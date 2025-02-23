96e73692cd45bf2c7883ca8b0d3dfff026795d6a
Aleksandr Kondratenko <madbee87@gmail.com>
2025-02-23 21:28:51 +0200

    commit

--- Trait.php
+++ Trait.php
@@ -2,21 +2,24 @@
 
 trait Trait1
 {
-    public function test():int{
+    public function test(): int
+    {
         return 1;
     }
 }
 
 trait Trait2
 {
-    public function test():int{
+    public function test(): int
+    {
         return 2;
     }
 }
 
 trait Trait3
 {
-    public function test():int{
+    public function test(): int
+    {
         return 3;
     }
 }
@@ -29,7 +32,8 @@ class Test
         Trait3::test as test3;
     }
 
-    public function getSum():int{
+    public function getSum(): int
+    {
         return $this->test() + $this->test2() + $this->test3();
     }
 }
--- index.php
+++ index.php
@@ -45,22 +45,23 @@ class ColorRGB
 
     private function validateColor(int $value): int
     {
-        if($value < 0 || $value > 255) {
+        if ($value < 0 || $value > 255) {
             throw new InvalidArgumentException("Ошибка: $value не является валидным цветом. Диапазон 0-255.");
         }
 
         return $value;
     }
 
-    public function getHex(): string {
+    public function getHex(): string
+    {
         return sprintf("#%02X%02X%02X", $this->red, $this->green, $this->blue);
     }
 
     public function equals(ColorRGB $color): bool
     {
         return $this->red === $color->getRed() &&
-               $this->green === $color->getGreen() &&
-               $this->blue === $color->getBlue();
+            $this->green === $color->getGreen() &&
+            $this->blue === $color->getBlue();
     }
 
     public static function random(): ColorRGB
@@ -68,7 +69,8 @@ class ColorRGB
         return new ColorRGB(rand(0, 255), rand(0, 255), rand(0, 255));
     }
 
-    public static function mix(ColorRGB $color1, ColorRGB $color2, ColorRGB $color3): ColorRGB {
+    public static function mix(ColorRGB $color1, ColorRGB $color2, ColorRGB $color3): ColorRGB
+    {
         $red = intval(round(($color1->getRed() + $color2->getRed() + $color3->getRed()) / 3));
         $green = intval(round(($color1->getGreen() + $color2->getGreen() + $color3->getGreen()) / 3));
         $blue = intval(round(($color1->getBlue() + $color2->getBlue() + $color3->getBlue()) / 3));
